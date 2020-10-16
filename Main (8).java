f920cbd18cfb125aca327b26927ee06c9705a576
Brundha-JN <68340169+Brundha-JN@users.noreply.github.com>
2020-10-17 00:06:51 +0530

    Day 5 Assignment

---
+++ Main (8).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class anchor {
+    Scanner sc=new Scanner(System.in);
+    String name;
+    int score=0;
+
+    public void getDetails(){
+
+        System.out.println("Enter anchor name");
+        name=sc.next();
+
+    }
+
+}
